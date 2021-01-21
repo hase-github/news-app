05e3679e8e0e0b132048fd444a502e96de539ca4
hase-github <48000.10mai@gmail.com>
2021-01-21 21:51:06 +0900

    20210121_2

--- App.js
+++ App.js
@@ -1,44 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, FlatList, SafeAreaView } from "react-native";
+import ListItem from "./compornents/ListItem";
+import dummyArticles from "./dummies/articles";
+import Constants from "expo-constants";
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      {
-        //<View>はReact-nativeが標準で提供するコンポーネント。Htmlのdivタグみたいなもの
-      }
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: "https://picsum.photos/200/300" }}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.......
-          </Text>
-          <Text style={styles.subText}> ReactNews </Text>
-        </View>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
+//テンプレート文字列を使っている
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    //alignItems: "center",
+    //justifyContent: "center",
   },
   itemContainer: {
     height: 100,
@@ -65,3 +41,67 @@ const styles = StyleSheet.create({
     color: "gray",
   },
 });
+
+export default function App() {
+  /**  const items = articles.map((article, index) => {
+    return (
+      <ListItem
+        imageUrl={article.urlToImage}
+        title={article.title}
+        author={article.author}
+        key={index}
+        //keyを忘れずに
+      />
+    );
+  }); */
+  const [articles, setArticles] = useState([]);
+  //ststeを使う
+  //useStateの引数はstate(ここではarticles)の初期値
+  useEffect(() => {
+    fetchArticles();
+
+    //alert(Constants.manifest.extra.newsApiKey);
+    //app.jsonのextraを使って、NewsAPIのAPIKeyを仕込む
+    /*const timer = setTimeout(() => {
+      setArticles(dummyArticles);
+    }, 2000);
+    return () => clearTimeout(timer);*/
+    //コンポーネントがアンマウントされるときにクリーンアップが必要な場合は、
+    //returnでクリーンアップ関数を返す
+  }, []);
+  //useEffectを使う
+  //useEffectの第２引数の配列に書いた変数が変化したときだけ、第１引数の関数が動く
+  //空入れるにすると、最初に読み込まれた１回だけ処理される。
+
+  const fetchArticles = async () => {
+    //axiosでAPIを実行
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+      //console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {
+        //<View>はReact-nativeが標準で提供するコンポーネント。Htmlのdivタグみたいなもの
+        //SafeAreaViewを使うと、上下に余白ができる（iPhone11とかのM字の部分）
+      }
+
+      <FlatList //FlatListで一覧がスワイプできるようになる
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
+}
