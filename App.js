6386b06f3e3855450d16bf3be2fdb87c3560d0af
hase-github <48000.10mai@gmail.com>
2021-01-21 15:31:29 +0900

    git_test

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default function App() {
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            culpa qui officia deserunt mollit anim id est laborum.......
           </Text>
           <Text style={styles.subText}> ReactNews </Text>
         </View>
